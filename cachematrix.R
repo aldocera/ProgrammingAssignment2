32952f76beb1f04ef79197f1f022b2f3d3b9b53a
aldocera <kripton161@hotmail.com>
2015-06-18 12:41:20 -0500

    assignment ready

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Objective 1st function: 
+## to create a square matrix so that
+## the next function will evaluate if
+## it is necesary to compute its inverse.
 
-## Write a short comment describing this function
+### Objective 2nd function:
+## A check-up is done whether the inverse of 
+## the created matrix is already computed.
+## If it is, the stored inverse is printed.
+## If not, the inverse is calculated and returned.
+
+#----------------------------------------------------------
+
+## This function has the capability to create a matrix. 
+#  Likewise, a new matrix can be set within the function,
+#  resetting all values.
+#  The inverse's values can be set or checked also.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cache<-NULL
+  
+  set<- function(nmatrix){ 
+    x<<- nmatrix
+    cache<<- NULL
+  }
+  
+  getvalue<- function() {x}
+  setInverse<- function(inverse) {cache <<- inverse}
+  getInverse<- function () {cache}
+  
+  list(set = set, 
+       getvalue = getvalue, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix
+#  in case no inverse has been computed.
+#  Returns inverse when computation has been done before.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  cache<-x$getInverse()
+  
+  if(!is.null(cache)){
+    message("getting cache Inverse")
+    return(cache)
+    }
+  
+  inverse<-x$getvalue()
+  cache<- solve(inverse)
+  x$setInverse(cache)
+  
+  cache
+  
 }
